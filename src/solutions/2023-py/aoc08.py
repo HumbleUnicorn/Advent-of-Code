b60784409d337516343432e466cc0ea6eec38fcc
Ruth Wertz <rwertz@purdue.edu>
2023-12-18 00:07:21 -0500

    Day 8 - Solved :christmas_tree:

--- src/solutions/2023-py/aoc08.py
+++ src/solutions/2023-py/aoc08.py
@@ -26,12 +26,22 @@
 #                 if i at last pos of n, reset i to 0 ---> loop inst indefinitely
 #                 if new key == 'ZZZ', break ---> end loop, return counter value
 # 
-# 
-#
 # // PART 2 //
-# 
+#  
+# Reference: (https://www.w3schools.com/python/python_lists_comprehension.asp)
+# gist = [expression *for* item *in* iterable *if* condition == True]   
 #
-#        
+# ghost keys = filtered list of keys where last char = 'A'
+# 
+# First Plan 
+#     *update* change 1st key ---> keys = list of ghost keys  
+#     *update* change key = new key ---> keys = new keys 
+#     *update* change break condition ---> check if all keys end with 'Z'
+# Works, BUT simultaneous looping taking toooooo long... 
+#
+# Second Plan
+#     Get no. of steps for each ghost key to get to '**Z'---> solve for LCM
+#
 # =============================================================================
 
 
@@ -39,12 +49,16 @@
 #  //INPUT DATA//
 # =============================================================================
 
-test8=[]
-with open('inputs/t08.txt') as f: 
+test8a=[]
+with open('inputs/t08a.txt') as f: 
     for lines in f:
-        test8.append(lines)  
+        test8a.append(lines)  
         
-                
+test8b=[]
+with open('inputs/t08b.txt') as f: 
+    for lines in f:
+        test8b.append(lines)  
+                     
 day8=[]
 with open('inputs/d08.txt') as f: 
     for lines in f:
@@ -57,8 +71,9 @@ with open('inputs/d08.txt') as f:
 
 # from collections import Counter
 # imprt re
+import math
 
-def parseInst(data):
+def parseInst(data):                # return list of instructions as L = 0, R = 1
     line = data[0]
     line = line.replace('\n','')
     line = line.replace('L','0')
@@ -70,7 +85,7 @@ def parseInst(data):
     data.pop(0)
     return inst
 
-def parseMap(data):
+def parseMap(data):                 # return dict of map as {'KEY': [L,R]}
     map={}
     for line in data:
         line = line.replace('\n','')
@@ -82,18 +97,53 @@ def parseMap(data):
         map.update({line[0]:line[1]})
     return map
 
-def countSteps(inst,map):
+def ghostKeys(map):
+    key_list = list(map.keys())
+    filtered_keys = [item for item in key_list if item[2] == 'A']
+    return filtered_keys
+
+def countSteps(inst, map):          # count steps from key = 'AAA' to key = 'ZZZ', return c     
     key = 'AAA'
     c=0
     while True:
-        for i in range(len(inst)):
-            index=inst[i]
-            key = map.get(key)[index]
-            c+=1
-            if i == len(inst)-1:
+        for i in range(len(inst)):  
+            index=inst[i]                       # call intruction 
+            key = map.get(key)[index]           # call new key  
+            c+=1                                # increase counter
+            if i == len(inst)-1:                # check value of i, reset to 0 if last pos
                 i = 0
-        if key == 'ZZZ':
-            break   
+        if key == 'ZZZ':                        # check value of key, break if 'ZZZ'
+            break
+    return c
+
+def ghostSteps(inst, map, key):     # count steps from key = '**A' to key = '**Z', return c     
+    key_start = key
+    c=0
+    while True:
+        for i in range(len(inst)):  
+            index=inst[i]                       # call intruction 
+            key = map.get(key)[index]           # call new key  
+            c+=1                                # increase counter
+            if i == len(inst)-1:                # check value of i, reset to 0 if last pos
+                i = 0
+        if key[2] == 'Z':                       # check value of key, break if '**Z'
+            break
+    return c
+
+def OLDghostSteps(inst,map,keys):      # NOT USED
+    c=0
+    while True:
+        for i in range(len(inst)):              # loop through instructions
+            index = inst[i]                     # call instruction 
+            new = []
+            for key in keys:                    # call list of new keys
+                new.append(map.get(key)[index])        
+            keys = new                          # retain new keys    
+            c+=1                                # increase counter
+            if i == len(inst)-1:                # check value of i, reset to 0 if last pos
+                i = 0
+        if all(key[2] =='Z' for key in keys):   # check value of key, break if all end with 'Z'
+            break
     return c
 
 
@@ -101,15 +151,23 @@ def countSteps(inst,map):
 #  // TEST OUTPUT //
 # =============================================================================
 
-inst = parseInst(test8); #print(inst)
+inst = parseInst(test8a); #print(inst)
 
-map = parseMap(test8); #print(map)
+map = parseMap(test8a); #print(map)
 
-steps = countSteps(inst,map); print(steps)
-    
-    
-    
+steps = countSteps(inst, map); #print(steps)
 
+inst_b = parseInst(test8b); #print(inst_b)
+
+map_b = parseMap(test8b); #print(map_b)
+
+keys = ghostKeys(map_b); #print(keys)
+
+gSteps = []
+for key in keys:
+    gSteps.append(ghostSteps(inst_b, map_b, key))
+
+steps = math.lcm(*gSteps); #print(steps) 
    
 # =============================================================================
 #   // PUZZLE OUTPUT //
@@ -119,4 +177,15 @@ inst = parseInst(day8); #print(inst)
 
 map = parseMap(day8); #print(map)
 
-steps = countSteps(inst,map); print(steps)
+steps = countSteps(inst, map); #print(steps)
+
+keys = ghostKeys(map); print(keys)
+
+gSteps = []
+for key in keys:
+    gSteps.append(ghostSteps(inst, map, key))
+print(gSteps)
+
+steps = math.lcm(*gSteps); print(steps) 
+
+
